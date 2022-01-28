cb65bce0c07a064c7753efb84a14d4e183e5776f
Inoki <veyx.shaw@gmail.com>
2022-01-28 14:33:35 +0100

    Add precomputed roots version in CUDA (large mem)

--- DFT.cu
+++ DFT.cu
@@ -70,3 +70,71 @@ Error:
     cudaFree(d_in);
     cudaFree(d_out);
 }
+
+// Kernel definition
+__global__ void calculateDFTCUDAKernelWithPrecomputedRoot(cuComplex* in, cuComplex* roots, cuComplex* out, size_t num)
+{
+    int i = threadIdx.x;
+    if (i < num)
+    {
+        out[i].x = 0;
+        out[i].y = 0;
+        for (int j = 0; j < num; j++)
+        {
+            out[i] = cuCaddf(out[i], cuCmulf(in[j], roots[i * j % num]));
+        }
+    }
+}
+
+void calculateDFTCUDALargeMem(std::complex<float>* in, std::complex<float>* out, size_t num)
+{
+    if (num == 0) return;
+
+    // Allocate vectors in device memory
+    cuComplex* d_in;
+    cudaMalloc(&d_in, num * sizeof(cuComplex));
+    cuComplex* d_out;
+    cudaMalloc(&d_out, num * sizeof(cuComplex));
+    cuComplex* d_roots;
+    cudaMalloc(&d_roots, num * sizeof(cuComplex));
+
+    // Copy vectors from host memory to device memory
+    cudaMemcpy(d_in, in, num * sizeof(cuComplex), cudaMemcpyHostToDevice);
+
+    // Precompute DFT
+    cuComplex* roots = (cuComplex *)malloc(sizeof(cuComplex) * num);
+    for (int i = 0; i < num; i++)
+    {
+        std::complex<float> root = std::exp(std::complex<float>(0, -2 * M_PI * i / num));
+        roots[i] = make_cuComplex(root.real(), root.imag());
+    }
+    cudaMemcpy(d_roots, roots, num * sizeof(cuComplex), cudaMemcpyHostToDevice);
+    free(roots);
+
+    calculateDFTCUDAKernelWithPrecomputedRoot<<<1, num >>>(d_in, d_roots, d_out, num);
+
+    cudaError_t cudaStatus;
+
+    // Check for any errors launching the kernel
+    cudaStatus = cudaGetLastError();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "DFT Kernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
+        goto Error;
+    }
+
+    // cudaDeviceSynchronize waits for the kernel to finish, and returns
+    // any errors encountered during the launch.
+    cudaStatus = cudaDeviceSynchronize();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching DFT Kernel!\n", cudaStatus);
+        goto Error;
+    }
+
+    // Copy back the results
+    cudaMemcpy(out, d_out, num * sizeof(cuComplex), cudaMemcpyDeviceToHost);
+
+Error:
+    cudaFree(d_in);
+    cudaFree(d_out);
+    cudaFree(d_roots);
+}
--- DFT.h
+++ DFT.h
@@ -5,4 +5,5 @@
 void calculateDFT(std::complex<float> *in, std::complex<float> *out, size_t num);
 #ifdef __HAS_CUDA__
 void calculateDFTCUDA(std::complex<float>* in, std::complex<float>* out, size_t num);
+void calculateDFTCUDALargeMem(std::complex<float>* in, std::complex<float>* out, size_t num);
 #endif
